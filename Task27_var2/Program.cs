638a89653185fdb0b4a9d7c09a7ed28f9da0d6eb
TatShar <tvku@yandex.ru>
2022-08-16 21:53:34 +0300

    Task27_Var2

---
+++ Task27_var2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberT = Convert.ToString(number);
+int sum = 0;
+
+int length = numberT.Length;
+for (int i = 0; i<length; i++) {
+sum = sum + numberT[i];
+}
+
+Console.Write(sum);
+
